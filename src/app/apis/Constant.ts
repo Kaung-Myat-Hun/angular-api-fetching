d6b73fe7d2618456949e78d33cc97829347919f1
kaung <kaungmyathun7@gmail.com>
2023-04-24 16:00:12 +0630

    update

--- src/app/apis/Constant.ts
+++ src/app/apis/Constant.ts
@@ -1,3 +1,3 @@
 export const FetchUsersAPI :string  = 'https://jsonplaceholder.typicode.com/users'
 
-export const GetUserDetailAPI : any = (id:number)=> `https://jsonplaceholder.typicode.com/users/${id}`
+export const GetUserDetailAPI = (id:number)=> `https://jsonplaceholder.typicode.com/users/${id}`
